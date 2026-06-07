56efb0de8744ac896f9d6f99659e33a3dd5f6271
vk57434 <vk4492020@gmail.com>
2026-06-08 01:59:53 +0530

    fix network error issues

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,5 @@
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const defaultUrl = import.meta.env.DEV
+  ? "http://localhost:5000/api"
+  : "https://edtech-kpvy.onrender.com/api";
+
+export const API_URL = import.meta.env.VITE_API_URL || defaultUrl;
