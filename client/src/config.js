f3832c7e71f49935b9f6d055995c5fea1aa7babc
vk57434 <vk4492020@gmail.com>
2026-06-08 02:30:57 +0530

    config .js fix issue

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const defaultUrl = import.meta.env.DEV
   ? "http://localhost:5000/api"
-  : "https://edtech-kpvy.onrender.com/api";
+  : "https://edtech-54bf.onrender.com/api";
 
 export const API_URL = import.meta.env.VITE_API_URL || defaultUrl;
