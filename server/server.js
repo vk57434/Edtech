c230d3977c669a524b4f42ef7886af306b3157c0
vk57434 <vk4492020@gmail.com>
2026-06-10 23:37:13 +0530

    fix cors

--- server/server.js
+++ server/server.js
@@ -26,20 +26,20 @@ const allowedOrigins = [
 ];
 
 // Middlewares
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    credentials: true,
-  })
-);
-app.options("*", cors());
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Backend running"));
